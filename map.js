a21d28c685d32fa99fcf01d6bc7ccab720bb27b7
Alex <jalexlopez15@gmail.com>
2020-08-25 00:20:08 -0700

    new map api

--- map.js
+++ map.js
@@ -1,8 +1,31 @@
-let map;
+function addMarker(map) {
+    var glasshouseMarker = new H.map.Marker({ lat: 33.982709, lng: -117.339323 });
+    map.addObject(glasshouseMarker);
+}
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 33.982758, lng: -117.339297 },
-        zoom: 10
+function moveMapToRiverside(map) {
+    map.setCenter({ lat: 33.982709, lng: -117.339323 });
+    map.setZoom(16);
+}
+
+var platform = new H.service.Platform({
+    apikey: 'jOHJ-Z4Au-UnL3-x5iv11B30XSevt27X7Ox3lV3QZCk'
+});
+var defaultLayers = platform.createDefaultLayers();
+
+var map = new H.Map(document.getElementById('mapdiv'),
+    defaultLayers.vector.normal.map, {
+        center: { lat: 50, lng: 5 },
+        zoom: 15,
+        pixelRatio: window.devicePixelRatio || 1
     });
+
+window.addEventListener('resize', () => map.getViewPort().resize());
+
+var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+var ui = H.ui.UI.createDefault(map, defaultLayers);
+window.onload = function() {
+    addMarker(map);
+    moveMapToRiverside(map);
 }
